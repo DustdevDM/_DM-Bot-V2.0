7f127b257746eb1f5c4c18378cf1ddcb560cce73
DustinDEV2more <Dustin@Dustin-DM.de>
2020-11-07 16:59:09 +0100

    fix last comit

--- updatedocker.sh
+++ updatedocker.sh
@@ -1,5 +1,5 @@
 git fetch
 git pull
-docker build -t dustin/_dm .
+docker build -t dustin/dm-bot .
 docker rm --force DM-Twitch-Bot
-docker run -d --restart always --cap-add=SYS_ADMIN --name DM-Twitch-Bot -p 6787:6787 dustin/_dm
+docker run -d --restart always --cap-add=SYS_ADMIN --name DM-Twitch-Bot -p 6787:6787 dustin/bot
